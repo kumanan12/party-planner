ab42aa63236460ce44cf36344f429a3649251793
kumanan12 <kumanan12@yahoo.com>
2015-04-11 15:06:49 -0400

    Facebook and google login working.

--- party-planner.js
+++ party-planner.js
@@ -14,6 +14,24 @@ if (Meteor.isClient) {
       Session.set('counter', Session.get('counter') + 1);
     }
   });
+
+  Template.login.events({
+    'click #facebook-login': function(event) {
+      Meteor.loginWithFacebook({}, function(err){
+        if (err) {
+          throw new Meteor.Error("Facebook login failed");
+        }
+      });
+    },
+
+    'click #logout': function(event) {
+      Meteor.logout(function(err){
+        if (err) {
+          throw new Meteor.Error("Logout failed");
+        }
+      })
+    }
+  });
 }
 
 if (Meteor.isServer) {
---
+++ server/social-config.js
@@ -0,0 +1,13 @@
+/**
+ * Created by Kumanan on 4/11/2015.
+ */
+ServiceConfiguration.configurations.remove({
+    service: 'facebook'
+});
+
+ServiceConfiguration.configurations.insert({
+    service: 'facebook',
+    appId: '990714107613618',
+    secret: '9aef1645c4fdc93438393576886eb0a1'
+});
+
