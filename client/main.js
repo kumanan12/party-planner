c78bdb0c8d6e776a7616f3a9e7d3c86876196b19
kumanan12 <kumanan12@yahoo.com>
2015-04-11 16:04:58 -0400

    Organized code and directories.

---
+++ client/main.js
@@ -0,0 +1,35 @@
+// counter starts at 0
+Session.setDefault('counter', 0);
+
+Template.hello.helpers({
+    counter: function () {
+        return Session.get('counter');
+    }
+});
+
+Template.hello.events({
+    'click button': function () {
+        // increment the counter when button is clicked
+        Session.set('counter', Session.get('counter') + 1);
+    }
+});
+
+Template.login.events({
+    'click #facebook-login': function (event) {
+        Meteor.loginWithFacebook({}, function (err) {
+            if (err) {
+                throw new Meteor.Error("Facebook login failed");
+            }
+        });
+    },
+
+    'click #logout': function (event) {
+        Meteor.logout(function (err) {
+            if (err) {
+                throw new Meteor.Error("Logout failed");
+            }
+        })
+    }
+});
+
+
---
+++ server/startup.js
@@ -0,0 +1,6 @@
+/**
+ * Created by Kumanan on 4/11/2015.
+ */
+Meteor.startup(function () {
+    // code to run on server at startup
+});
