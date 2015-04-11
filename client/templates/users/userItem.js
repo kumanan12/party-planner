aec00f85cc1de72f48196de9341449086cd91dc1
kumanan12 <kumanan12@yahoo.com>
2015-04-11 16:47:19 -0400

    Basic Templating for users object done.

---
+++ client/templates/users/userItem.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Kumanan on 4/11/2015.
+ */
+//Template.userItem.helpers()
---
+++ client/templates/users/userList.js
@@ -0,0 +1,31 @@
+/**
+ * Created by Kumanan on 4/11/2015.
+ */
+
+var userList=[
+    {
+        _id:1,
+        createdAt:"1/1/2015",
+        profile:{
+            name:"Kumanan Murugesan"
+        }
+    },
+    {
+        _id:2,
+        createdAt:"3/11/2015",
+        profile:{
+            name:"Nannan Murugesan"
+        }
+    },
+    {
+        _id:1,
+        createdAt:"4/7/2015",
+        profile:{
+            name:"Chezhiyan Murugesan"
+        }
+    }
+];
+
+Template.userList.helpers({
+    users:userList
+});
